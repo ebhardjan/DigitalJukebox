de9ac5979aeaa382c72536707cf0f096b315fbec
Valentin Trifonov <valentin.trifonov@hotmail.de>
2017-09-16 04:32:57 +0200

    WIP: guest browser code

--- public/script/guest.js
+++ public/script/guest.js
@@ -1,24 +1,76 @@
-var $venueselection = $('#venueselection');
-var $editplaylist = $('#editplaylist');
+var $venuesview = null;
+var $playlistview = null;
+var id = null;
 var socket = null;
 var socketId = null;
 var location = null;
 
-function establishConnection() {
+function establishConnection(cb) {
+	id =
 	socket = io();
 	socket.on('connect', function() {
-		socketId = 
+		socketId = socket.id;
+		socket.on('setPlaylist', onSetPlaylist);
+		socket.on('availableHosts', onAvailableHosts);
+		cb();
 	});
 }
 
-function findVenuesNearby() {
-	$editplaylist.hide();
-	$venueselection.show();
-	$venueselection.html('Connecting...');
-
-
+function findLocation(cb) {
+	// TODO
+	location = 'defaultlocation';
+	cb();
 }
 
+function switchToVenuesMode() {
+	$playlistview.hide();
+	$venuesview.show();
+	$venuesview.html('Connecting...');
+
+	findLocation(function() {
+		establishConnection(function() {
+			findVenues();
+		});
+	});
+}
+
+function findVenues() {
+	socket.emit('registerGuest', {});
+}
+
+function onAvailableHosts(data) {
+	var $venues = $('#venueslist');
+	$venues.clear();
+	for (var i = 0; i < data.hosts.length; i++) {
+		(function(host){
+			var el = $('<div><a>name: ' + host.name + '</a></div>');
+			el.on('click', function() {
+				switchToPlaylistMode();
+				pushPickHost(host.id);
+			});
+			$venues.append(el);
+		})(data.hosts[i]);
+	}
+}
+
+function pushPickHost(id) {
+	socket.emit('pickHost', {host: id});
+}
+
+function switchToPlaylistMode() {
+	$venuesview.hide();
+	$playlistview.show();
+	$playlistview.html('Loading...');
+}
+
+function onSetPlaylist() { }
+
+function pushVote() { }
+
+function pushAddEntry() { }
+
 $(function(){
-	findVenuesNearby();
+	$venuesview = $('#venuesview');
+	$playlistview = $('#playlistview');
+	switchToVenuesMode();
 });
